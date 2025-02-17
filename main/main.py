71048e149c274d76fe2c7a70dff66179061e4dc5
Poprostulesny <mateusz.lesniczak@gmail.com>
2025-02-18 00:51:57 +0100

    asking which contest to choose

--- main/main.py
+++ main/main.py
@@ -20,7 +20,25 @@ browser.get(link+"\\login\\")
 login_name = browser.find_element(By.ID, "id_username")
 login_password = browser.find_element(By.ID, "id_password")
 login_button = browser.find_element(locate_with(By.TAG_NAME,"button").below({By.ID: "id_password"}))
+
+#logging in
 login_name.send_keys(user)
 login_password.send_keys(password)
 login_button.click()
 
+#asking the user which contest do they want to pull from
+table = browser.find_elements(By.CLASS_NAME, "toggle-cg")
+list_of_options = []
+for i in range(len(table)):
+    table[i].click()
+    if(i!=len(table)-1):
+        pom=browser.find_elements(locate_with(By.TAG_NAME,"a").below(table[i]).above(table[i+1]))
+    else:
+        pom=browser.find_elements(locate_with(By.TAG_NAME,"a").below(table[i]).above({By.TAG_NAME: "footer"}))
+    for t in pom:
+        list_of_options.append(t)
+
+print("Choose the number corresponding to the contest which answers you want to pull")
+for i in range(len(list_of_options)):
+    print(i,list_of_options[i].text)
+time.sleep(5)
