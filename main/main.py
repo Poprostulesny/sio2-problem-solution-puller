ff6ed64926167cb908f04744d5c8c8431c35096f
Poprostulesny <mateusz.lesniczak@gmail.com>
2025-02-25 19:45:58 +0100

    final

--- main/main.py
+++ main/main.py
@@ -25,10 +25,12 @@ settings = {"recentDestinations": [{"id": "Save as PDF", "origin": "local", "acc
 prefs = {'printing.print_preview_sticky_settings.appState': json.dumps(settings), "download.default_directory": dir_temp,"download.directory_upgrade": True,
     "download.prompt_for_download": False, "profile.default_content_settings.popups": 0}
 options.add_experimental_option('prefs', prefs)
+options.add_experimental_option("excludeSwitches", ["enable-logging"])
 browser = webdriver.Edge(options=options)
 browser.implicitly_wait(4)
 if not os.path.exists(dir_temp):
                 os.makedirs(dir_temp)
+os.system('cls')
 #searching for login
 browser.get(link+"\\login\\")
 login_name = browser.find_element(By.ID, "id_username")
@@ -39,7 +41,7 @@ login_button = browser.find_element(locate_with(By.TAG_NAME,"button").below({By.
 login_name.send_keys(user)
 login_password.send_keys(password)
 login_button.click()
-
+os.system('cls')
 #asking the user which contest do they want to pull from
 table = browser.find_elements(By.CLASS_NAME, "toggle-cg")
 list_of_options = []
@@ -108,12 +110,8 @@ print("Downloading and organizing files...")
 utils.create_filesystem(merge,dir,dir_temp,browser)
 
 
-# browser.get("https://sio2.staszic.waw.pl/c/matinf_k20_c/p/apt/1150/")
-# pdf = browser.execute_cdp_cmd("Page.printToPDF", {"printBackground": True})
-# pdf_data = base64.b64decode(pdf['data'])
-# pdf_path = dir + r"\\output.pdf"
-# with open(pdf_path, "wb") as f:
-#     f.write(pdf_data)
+
+
 
 
 
