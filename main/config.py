728d45d2fe23d88503d2d3069337862f23fcc78e
Poprostulesny <mateusz.lesniczak@gmail.com>
2025-02-26 01:15:20 +0100

    accidents happen

---
+++ main/config.py
@@ -0,0 +1,8 @@
+def fileconfig():
+    user = "abba"
+    password = "eeee"
+    link = "https://sio2.staszic.waw.pl/"
+    dir = r"C:\Users\jarek\Documents\Zadania SIO2"
+    download_folder_dir= r"C:\Users\jarek\Downloads"
+    #all files or only the best ones
+    return user, password, link, dir, download_folder_dir
