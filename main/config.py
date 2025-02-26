56177a4c9cc80f735a76f64099d44d20881aed0a
Poprostulesny <mateusz.lesniczak@gmail.com>
2025-02-26 01:38:39 +0100

    comments added

--- main/config.py
+++ main/config.py
@@ -2,7 +2,9 @@ def fileconfig():
     user = "abba"
     password = "eeee"
     link = "https://sio2.staszic.waw.pl/"
+    #directory to which all of the data will be downloaded
     dir = r"C:\Users\jarek\Documents\Zadania SIO2"
+    #your downloads folder
     download_folder_dir= r"C:\Users\jarek\Downloads"
-    #all files or only the best ones
+
     return user, password, link, dir, download_folder_dir
